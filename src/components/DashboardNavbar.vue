dcd4307cb67e3e66e4812aa60ce1e1616ff1f6f1
tjwaterman99@gmail.com <tjwaterman99@gmail.com>
2020-03-21 23:52:23 -0700

    Change order

--- src/components/DashboardNavbar.vue
+++ src/components/DashboardNavbar.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
 .dashboard-navbar.d-flex.flex-column.p-3.bg-primary
 	side-navbar-item(to="/dashboard" text="Experiments" icon="book-half-fill")
-	side-navbar-item(to="/dashboard/logs" text="Logs" icon="document-text")
 	side-navbar-item(to="/dashboard/reports" text="Reports (todo)" icon="table")
+	side-navbar-item(to="/dashboard/logs" text="Logs" icon="document-text")
 	side-navbar-item(to="/docs" text="Docs (todo)" icon="terminal")
 	side-navbar-item(to="/dashboard/account" text="Account" icon="person-fill").mb-auto
+
 	span.d-flex.justify-content-between.align-items-center.px-2
 		p.mr-4.p-1.text-white View staging data
 		b-form-checkbox(switch plain=true @change="toggleTestData" :checked="$root.environment!='production'").pb-3
-
 	side-navbar-item(to="/" text="Exit" icon="box-arrow-left")
 </template>
 
