3f2234a57358c763d1eb8458a7ac5d481659f0b2
tjwaterman99@gmail.com <tjwaterman99@gmail.com>
2020-03-22 10:27:27 -0700

    Rework doc links

--- src/components/DocsNavbar.vue
+++ src/components/DocsNavbar.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
 .dashboard-navbar.d-flex.flex-column.p-3.bg-secondary
-	side-navbar-item(to="/docs" text="Getting started" icon="house-fill" variant="secondary")
-	side-navbar-item(to="/docs/api-tokens" text="Experiment setup" icon="document-text" variant="secondary")
-	side-navbar-item(to="/docs/api-tokens" text="API authentication" icon="document-text" variant="secondary")
-	side-navbar-item(to="/docs/logging" text="Collecting data" icon="document-text" variant="secondary")
+	side-navbar-item(to="/docs" text="Getting started" icon="terminal" variant="secondary")
+	side-navbar-item(to="/docs/how-it-works" text="How it works" icon="text-indent-right" variant="secondary")
+	side-navbar-item(to="/docs/api-tokens" text="API authentication" icon="shield-shaded" variant="secondary")
+	side-navbar-item(to="/docs/create-experiment" text="Experiment setup" icon="book-half-fill" variant="secondary")
+	side-navbar-item(to="/docs/logging" text="Logging data" icon="document-text" variant="secondary")
 	side-navbar-item(to="/docs/reports" text="Calculating results" icon="table" variant="secondary").mb-auto
 
 	side-navbar-item(to="/dashboard" text="Dashboard" icon="book-half-fill" variant="secondary")
