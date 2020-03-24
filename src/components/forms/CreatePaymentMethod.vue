d819228de5ce40990807f9a0e94bbb83c938573a
tjwaterman99@gmail.com <tjwaterman99@gmail.com>
2020-03-23 21:36:38 -0700

    Add note about inputting stripe elements

--- src/components/forms/CreatePaymentMethod.vue
+++ src/components/forms/CreatePaymentMethod.vue
@@ -5,19 +5,25 @@
       h1 Add a payment
 
       .credit-card-inputs
-        card-number.mb-2.border.stripe-element.card-number(ref='cardNumber', :stripe='stripe', :options='options', @change='number = $event.complete')
-        card-expiry.mb-2.border.stripe-element.card-expiry(ref='cardExpiry', :stripe='stripe', :options='options', @change='expiry = $event.complete')
-        card-cvc.mb-2.border.stripe-element.card-cvc(ref='cardCvc', :stripe='stripe', :options='options', @change='cvc = $event.complete')
+        .mb-2.border.stripe-element.card-number(ref='cardNumber', :stripe='stripe', :options='options', @change='number = $event.complete')
+        .mb-2.border.stripe-element.card-expiry(ref='cardExpiry', :stripe='stripe', :options='options', @change='expiry = $event.complete')
+        .mb-2.border.stripe-element.card-cvc(ref='cardCvc', :stripe='stripe', :options='options', @change='cvc = $event.complete')
 </template>
 
 <script>
-import { CardNumber, CardExpiry, CardCvc } from 'vue-stripe-elements-plus'
+// Previous version:
+// https://github.com/tjwaterman99/quicksplit-api/blob/5b82b3e072f31ad1be4c3e7f860f39395ca26591/www/src/components/pages/Payments.vue
 
-// eslint-disable-next-line no-undef
-console.log(Stripe)
-
+// It looks like the stripe elements will automatically bind to elements
+// based on their id. So we need something like div#card-cvc for the stripe
+// element to bind to. 
+//
 // eslint-disable-next-line no-undef
 var stripe = new Stripe(process.env.VUE_APP_STRIPE_PUBLIC_KEY)
+var elements = stripe.elements()
+// var cardExpiryElement = elements.create('cardExpiry');
+// var cardCvcElement = elements.create('cardCvc');
+var cardNumberElement = elements.create('cardNumber');
 
 export default {
   props: ['options' ],
@@ -28,93 +34,12 @@ export default {
       number: false,
       expiry: false,
       cvc: false,
-      stripe: stripe
+      stripe: stripe,
+      paymentSetupIntent: undefined
     }
   },
-  components: { CardNumber, CardExpiry, CardCvc },
   methods: {
-    update () {
-      this.complete = this.number && this.expiry && this.cvc
-
-      // field completed, find field to focus next
-      if (this.number) {
-        if (!this.expiry) {
-          this.$refs.cardExpiry.focus()
-        } else if (!this.cvc) {
-          this.$refs.cardCvc.focus()
-        }
-      } else if (this.expiry) {
-        if (!this.cvc) {
-          this.$refs.cardCvc.focus()
-        } else if (!this.number) {
-          this.$refs.cardNumber.focus()
-        }
-      }
-      // no focus magic for the CVC field as it gets complete with three
-      // numbers, but can also have four
-    }
-  },
-  watch: {
-    number () { this.update() },
-    expiry () { this.update() },
-    cvc () { this.update() }
-  }
-}
-</script>
-
-<style>
-.credit-card-inputs.complete {
-  border: 2px solid green;
-}
-</style>
-
-<!-- <template>
-	<div>
-		<Header>Add credit card</Header>
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-6 offset-lg-3">
-			<form class="payment-method">
-				<div class="form-group">
-					<div id="card-number" ></div>
-				</div>
-				<div class="form-row">
-					<div class="col">
-						<div id="card-cvc"></div>
-					</div>
-					<div class="col">
-						<div id="card-expiry"></div>
-					</div>
-				</div>
-				<div class="form-group">
-					<button type="submit" class="btn btn-primary btn-block submit" @click="handleAddPayment">Add credit card</button>
-				</div>
-			</form>
-			</div>
-		</div>
-	</div>
-	</div>
-</template>
-
-<script>
-var stripe = new Stripe('pk_test_vs6w4emCv9szUa8mJyeXKTey00IV5800C2');
-var elements = stripe.elements()
-var cardExpiryElement = elements.create('cardExpiry');
-var cardCvcElement = elements.create('cardCvc');
-var cardNumberElement = elements.create('cardNumber');
-
-export default {
-	name: "CreatePaymentMethod",
-	data: function() {
-		return {
-			cardNumberComplete: false,
-			cardExpiryComplete: false,
-			cardCvcComplete: false,
-			paymentSetupIntent: null
-		}
-	},
-	methods: {
-		handleAddPayment: function(event) {
+    handleAddPayment: function(event) {
 			event.preventDefault()
 			var that = this;
 			stripe.confirmCardSetup(
@@ -137,7 +62,45 @@ export default {
 				}
 			})
 		}
+  },
+  watch: {},
+	created: function() {
+		var that = this
+		this.$api.get('/account/payment-setup').then(function(resp) {
+			that.paymentSetupIntent = resp.data.data
+		}).catch(function(error) {
+			console.log(error)
+		})
+	}
+
+}
+</script>
+
+<style>
+.credit-card-inputs.complete {
+  border: 2px solid green;
+}
+</style>
+
+<!--
+<script>
+var stripe = new Stripe('pk_test_vs6w4emCv9szUa8mJyeXKTey00IV5800C2');
+var elements = stripe.elements()
+var cardExpiryElement = elements.create('cardExpiry');
+var cardCvcElement = elements.create('cardCvc');
+var cardNumberElement = elements.create('cardNumber');
+
+export default {
+	name: "CreatePaymentMethod",
+	data: function() {
+		return {
+			cardNumberComplete: false,
+			cardExpiryComplete: false,
+			cardCvcComplete: false,
+			paymentSetupIntent: null
+		}
 	},
+
 	created: function() {
 		var that = this
 		this.$api.get('/account/payment-setup').then(function(resp) {
