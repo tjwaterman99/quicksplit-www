23dba8dafe73f15b6ae728325cec7b45d0894c90
tjwaterman99@gmail.com <tjwaterman99@gmail.com>
2020-03-27 15:33:00 -0700

    Try sorting plans view

--- src/components/pricing/PlanCollection.vue
+++ src/components/pricing/PlanCollection.vue
@@ -4,9 +4,8 @@
 		input(type="checkbox" checked).custom-control-input#annual
 		label.custom-control-label(for="annual", @click="toggleAnnual") Annual Prices
 	ul.plan-collection-list.list-group.list-group-horizontal-md.bg-light.border-0.d-flex.px-3.mt-2
-		li(v-for="plan in selectedPlans", :key="plan.id").plan-collection-item.list-group-item.bg-light.flex-even.p-0.border-0
+		li(v-for="plan in sortedPlans", :key="plan.id").plan-collection-item.list-group-item.bg-light.flex-even.p-0.border-0
 			plan-item(:plan="plan", :annual="annual")
-
 </template>
 
 <script>
@@ -29,6 +28,15 @@ export default {
 		}
 	},
 	computed: {
+		sortedPlans: function() {
+			var plans = this.selectedPlans
+			var sorted_plans = plans.sort( (f,s) => {
+				console.log(`First is ${f.name} second is ${s.name}. First is less = ${f.rank < s.rank}`)
+				return f.rank > s.rank
+			})
+			console.log(sorted_plans)
+			return sorted_plans
+		},
 		selectedPlans: function() {
 			if (this.annual) {
 				return this.$root.plans.filter(function(plan) {
